16f65d5eb6e496247253e840795c9978b1340e5d
Asif Ihsan <asifihsan0369@gmail.com>
2023-12-09 20:43:33 +0530

    Added SQRT Function

--- pythoncalc.py
+++ pythoncalc.py
@@ -1,5 +1,11 @@
+import math
 def additon():
     x=int(input("Enter the 1st number: "))
     y=int(input("Enter the 2nd number: "))
     s=x+y
     print(f"The sum  is {s}")
+
+def sqrt():
+    x=int(input("Enter a number: "))
+    s=math.sqrt(x)
+    print("Square Root is ",s)
