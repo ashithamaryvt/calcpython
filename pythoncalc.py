684830e279493b78d0e69420878579370c9b8ac3
Amjad <amjadkj26523@gmail.com>
2023-12-09 21:01:00 +0530

    division added

--- pythoncalc.py
+++ pythoncalc.py
@@ -9,3 +9,8 @@ def sqrt():
     x=int(input("Enter a number: "))
     s=math.sqrt(x)
     print("Square Root is ",s)
+def div():
+    x=int(input("Enter the 1st number: "))
+    y=int(input("Enter the 2nd number: "))
+    s=x/y
+    print(f"The division  is {s}")
